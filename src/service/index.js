26eb13c62d90e8c84f6979289190d08cc2c14742
guilhermetcltransporte <95231918+guilhermetcltransporte@users.noreply.github.com>
2024-12-03 09:55:59 -0300

    dev

--- src/service/index.js
+++ src/service/index.js
@@ -10,7 +10,7 @@ export class Service {
 
         let config = {};
 
-        let authorization = JSON.parse(localStorage.getItem('Authorization'));
+        let authorization = JSON.parse(localStorage.getItem('Authorization'))
 
         try {
 
@@ -22,42 +22,42 @@ export class Service {
                 }
             }
 
-            var response = await axios.post(this.url + path, data || {}, config);
+            var response = await axios.post(this.url + path, data || {}, config)
 
             if (authorization) {
-                authorization.lastAcess = response.headers['last-acess'];
-                authorization.expireIn = parseInt(response.headers['expire-in']);
-                localStorage.setItem('Authorization', JSON.stringify(authorization));
+                authorization.lastAcess = response.headers['last-acess']
+                authorization.expireIn = parseInt(response.headers['expire-in'])
+                localStorage.setItem('Authorization', JSON.stringify(authorization))
             }
 
-            return response;
+            return response
 
         } catch (error) {
 
             //API fora do ar
             if (error.code == 'ERR_NETWORK') {
-                const message = '[300] - Servidor fora do ar!';
-                Swal.fire({showCloseButton: true, title: 'Ops...', icon: 'error', text: message});
-                throw new Error(message);
+                const message = '[300] - Servidor fora do ar!'
+                Swal.fire({showCloseButton: true, title: 'Ops...', icon: 'error', text: message})
+                throw new Error(message)
             }
 
             //Em manutenção
             if (error?.response?.status == 301) {
-                const message = '[301] - Servidor em manutenção, aguarde um instante!';
-                Swal.fire({showCloseButton: true, title: 'Ops...', icon: 'warning', text: message, confirmButtonColor: "#FFF", confirmButtonText: 'Aguarde um instante'});
-                throw new Error(message);
+                const message = '[301] - Servidor em manutenção, aguarde um instante!'
+                Swal.fire({showCloseButton: true, title: 'Ops...', icon: 'warning', text: message, confirmButtonColor: "#FFF", confirmButtonText: 'Aguarde um instante'})
+                throw new Error(message)
             }
 
             //Sessão expirada
             if (error?.response?.status == 400) {
-                localStorage.removeItem('Authorization');
-                const to = window.location.hash.slice(1);
-                window.location.href = `/sign-in`;
-                throw new Error(error.response.data.message);
+                localStorage.removeItem('Authorization')
+                const to = window.location.hash.slice(1)
+                window.location.href = `/sign-in`
+                throw new Error(error.response.data.message)
             }
 
             //Erro desconhecido
-            const message = '[500] - Ocorreu um erro inesperado!';
+            const message = '[500] - Ocorreu um erro inesperado!'
             Swal.fire({showCloseButton: true, title: 'Ops...', icon: 'error', text: message, confirmButtonColor: "#FFF", confirmButtonText: '<span style="color: rgba(88, 86, 214)">Quero abrir um chamado!</span>',
             }).then((result) => {
                 if (result.isConfirmed) {
