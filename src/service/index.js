15d1776cd5bfec861b91cb46ccc26187f31be86b
guilhermetcltransporte <95231918+guilhermetcltransporte@users.noreply.github.com>
2024-12-05 15:22:28 -0300

    dev

--- src/service/index.js
+++ src/service/index.js
@@ -61,6 +61,9 @@ export class Service {
 
             //Erro desconhecido
             const message = '[500] - Ocorreu um erro inesperado!'
+            Swal.fire({showCloseButton: true, title: 'Ops...', icon: 'error', text: message, confirmButtonColor: "#FFF"});
+            /*
+            const message = '[500] - Ocorreu um erro inesperado!'
             Swal.fire({showCloseButton: true, title: 'Ops...', icon: 'error', text: message, confirmButtonColor: "#FFF", confirmButtonText: '<span style="color: rgba(88, 86, 214)">Quero abrir um chamado!</span>',
             }).then((result) => {
                 if (result.isConfirmed) {
@@ -68,6 +71,7 @@ export class Service {
                     Swal.fire('', 'Chamado <b>#49812</b> aberto com sucesso!', 'success');
                 }
             });
+            */
             throw new Error(message);
 
         }
